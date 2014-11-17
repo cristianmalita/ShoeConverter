62e6504c6f832a233a0f4c7fa437c6c1f1ed2927
Cristian Malita <cristianmalita@iMac-Cristian.local>
2014-11-17 19:12:06 +0200

    First rev of the Simple shoe size converter app

--- Shoe Converter/ViewController.swift
+++ Shoe Converter/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menShoeSizeTextField: UITextField!
+    @IBOutlet weak var menShoeSizeResultLabel: UILabel!
+    
+    @IBOutlet weak var womenShoeSizeTextField: UITextField!
+    @IBOutlet weak var womenShoeSizeResultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertMenShoeSizeButtonPressed(sender: UIButton) {
+        
+        let usShoeSize = menShoeSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        let euroShoeSize = usShoeSize + conversionConstant
+        
+        menShoeSizeResultLabel.hidden = false
+        menShoeSizeResultLabel.text = "\(euroShoeSize) in European size."
+        menShoeSizeTextField.text = ""
+        
+        
+    }
 
+    @IBAction func convertWomenShoeSizeButtonPressed(sender: UIButton) {
+        
+        let usShoeSize = Double((womenShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        let euroShoeSize = usShoeSize + conversionConstant
+        
+        womenShoeSizeResultLabel.hidden = false
+        womenShoeSizeResultLabel.text = "\(euroShoeSize) in European size."
+        womenShoeSizeTextField.text = ""
+        
+    }
 }
 
